48649a3a28859f7206a1d45d5ee1a96dadb7f952
Theekshana <47950733+wasthTheekshana@users.noreply.github.com>
2022-03-04 22:49:49 +0530

    Add Singleton Double checking way

--- Design Patterns/src/SingletonPattern/ParcticeCode/SingletonClass.java
+++ Design Patterns/src/SingletonPattern/ParcticeCode/SingletonClass.java
@@ -2,14 +2,26 @@ package SingletonPattern.ParcticeCode;
 
 public class SingletonClass {
 
-    private static volatile SingletonClass singletonClass = new SingletonClass();
+//    private static volatile SingletonClass singletonClass = new SingletonClass();
+
+    private static volatile SingletonClass singletonClass;
 
     private SingletonClass() {
-
+        if (singletonClass != null) {
+            throw new RuntimeException("Please Use getSingletonClass method");
+        }
     }
 
     public static SingletonClass getSingletonClass() {
 
+        if (singletonClass == null) {
+            synchronized (SingletonClass.class) {
+                if (singletonClass == null) {
+                    singletonClass = new SingletonClass();
+
+                }
+            }
+        }
         return singletonClass;
     }
 }
