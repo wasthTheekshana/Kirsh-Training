5309af9cd4001e2135cc013af60185f6f2dfdd74
Theekshana <47950733+wasthTheekshana@users.noreply.github.com>
2022-03-06 22:03:01 +0530

    update

--- CH_WordCounter/src/main/java/FirstWay.java
+++ CH_WordCounter/src/main/java/FirstWay.java
@@ -3,24 +3,32 @@ import java.util.Scanner;
 public class FirstWay {
     public static void main(String[] args) {
         String message ;
-        String checkLetter;
 
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter Your Word :");
         message = sc.nextLine();
 
-        System.out.println("Enter letter want you want check : ");
-        checkLetter = sc.nextLine();
 
+        int[] freq = new int[message.length()];
         char[] messageArray = message.toCharArray();
-        char c = checkLetter.charAt(0);
-        int letterCount = 0;
+
         for (int i=0; i< message.length();i++){
-            if (messageArray[i] == c){
-                letterCount++;
+            freq[i] = 1;
+            for (int j = i+1; j<message.length();j++){
+                if (messageArray[i] == messageArray[j]){
+                    freq[i]++;
+                    messageArray[j] = '0';
+                }
+            }
+
+        }
+
+        for (int i =0;i< freq.length;i++){
+            if(messageArray[i] != ' ' && messageArray[i] !='0'){
+                System.out.println(messageArray[i] + "-------" + freq[i]);
             }
         }
-        System.out.println("The Letter " + checkLetter + " Count is " + letterCount);
+//        System.out.println("The Letter " + checkLetter + " Count is " + letterCount);
 
     }
 }
--- Design Patterns/src/SingletonPattern/ParcticeCode/Test.java
+++ Design Patterns/src/SingletonPattern/ParcticeCode/Test.java
@@ -3,9 +3,9 @@ package SingletonPattern.ParcticeCode;
 public class Test {
     public static void main(String[] args) {
         SingletonClass c1 = SingletonClass.getSingletonClass();
-        System.out.println(c1);
-
         SingletonClass c2 = SingletonClass.getSingletonClass();
+
+        System.out.println(c1);
         System.out.println(c2);
     }
 }
